f069a16706f6551da523a2bb3189189dc1efb807
danillonc <danillonc@live.com>
2017-10-26 16:22:52 -0200

    validation fields in controller

--- app/controllers/cadastro.js
+++ app/controllers/cadastro.js
@@ -1,5 +1,5 @@
 module.exports.cadastro = function(application, req, res){
-   res.render('cadastro');
+   res.render('cadastro',{validacao: {}, dadosForm: {}});
 }
 
 module.exports.cadastrar = function(application, req, res){
@@ -10,5 +10,14 @@ module.exports.cadastrar = function(application, req, res){
     req.assert('usuario', 'Usuário não pode ser vazio.').notEmpty();
     req.assert('senha', 'Senha não pode ser vazio.').notEmpty();
     req.assert('casa', 'Casa não pode ser vazio.').notEmpty();
+
+    var erros = req.validationErrors();
+
+    if(erros){
+    	res.render('cadastro',{validacao: erros, dadosForm: dadosForm});
+    	return;
+    }
+
+    res.send('Podemos cadastrar.')
 
 }
